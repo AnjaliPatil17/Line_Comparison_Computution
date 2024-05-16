b03354345b76096662bceb59e0158b79ec107244
Anjali <anjaliap1227@gmail.com>
2024-05-16 12:33:43 +0530

    Line_Comparison_UC1-Length_of_line file add

--- src/Line_Comparison.java
+++ src/Line_Comparison.java
@@ -1,5 +1,16 @@
+
 public class Line_Comparison {
+
+    public void isEqual(double x1,double x2,double y1,double y2){
+        double length_line = Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+        System.out.println("Length of line is :"+length_line);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation");
+
+        Line_Comparison uc1 = new Line_Comparison();
+        uc1.isEqual(1, 5, 4, 9);
+
+
     }
 }
